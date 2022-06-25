d6c82ac25bb275f09229bd189c84d0fc17b43d2c
Saheb Giri <iamsahebgiri@gmail.com>
2022-06-25 21:01:04 +0530

    fix: get port from env

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,5 +1,5 @@
 export const ISDEV = process.env.NODE_ENV === "development";
-export const PORT = 8000;
+export const PORT = process.env.PORT || 80;
 export const dbUri = ISDEV
   ? "mongodb://localhost:27017/test"
   : `mongodb+srv://admin:${process.env.DB_PASS}@cluster0.exwma.mongodb.net/?retryWrites=true&w=majority`;
