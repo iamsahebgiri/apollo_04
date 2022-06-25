b692d95a87834ffdf67636bd497882713cdee88c
Saheb Giri <iamsahebgiri@gmail.com>
2022-06-25 21:17:09 +0530

    draft: add main search bar

---
+++ app/lib/home/main_home.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class MainHomePage extends StatelessWidget {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        color: Colors.blueGrey.shade50,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 50.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: Colors.white),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.menu,
+                          color: Colors.deepPurple,
+                        ),
+                        onPressed: () {
+                          print("your menu action here");
+                          _scaffoldKey.currentState!.openDrawer();
+                        },
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "Search places, restaurants",
+                              border: InputBorder.none,
+                              hintStyle: TextStyle(color: Colors.grey)),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.account_balance_wallet_rounded,
+                          color: Colors.deepPurple,
+                        ),
+                        onPressed: () {
+                          print("your menu action here");
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+            // Text("Hello")
+          ],
+        ),
+      ),
+    );
+  }
+}
