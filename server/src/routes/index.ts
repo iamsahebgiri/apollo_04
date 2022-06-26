1082057e60dba07aa3f16661b755aa38e6b0c882
Krishna Mahato <krishhtrishh9304@gmail.com>
2022-06-26 11:02:02 +0530

    add: transact, hotels and restaurants main route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,17 @@
 import Router, { Application } from "express";
 import guideRouter from "./guide.route";
+import hotelsRouter from "./hotels.route";
 import placesRouter from "./places.route";
+import restaurantsRouter from "./restaurants.route";
+import TransactRouter from "./transact.route";
 import userRouter from "./user.route";
 const mainRouter: Application = Router();
 
 mainRouter.use("/user", userRouter);
 mainRouter.use("/places", placesRouter);
+mainRouter.use("/restaurants", restaurantsRouter);
+mainRouter.use("/hotels", hotelsRouter);
+mainRouter.use("/transact", TransactRouter);
 mainRouter.use("/guide", guideRouter);
 
 export default mainRouter;
